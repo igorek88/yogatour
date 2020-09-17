be5b8aff18ab92df1727a8adc40ded58056752bc
igorek88 <konrad88@bigmir.net>
2020-09-17 18:35:58 +0300

    first commit

---
+++ src/blocks/modules/reasons/reasons.js
@@ -0,0 +1,13 @@
+modules.define('reasons', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
